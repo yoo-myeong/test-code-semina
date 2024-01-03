8f509c10e686d927a8c0aa4a24deeeb7bfae9cd2
sceonn <yoou201@naver.com>
2024-01-03 23:22:59 +0900

    고전파 서비스 테스트

--- DB/service.test.ts
+++ DB/service.test.ts
@@ -1,11 +1,11 @@
 import sinon from 'sinon';
 import { Repository } from './repository';
 import { Service } from './service';
+import { Database } from './database';
 
 describe('service', () => {
   it('전부 가져온다', () => {
-    const repository = sinon.createStubInstance(Repository)
-    repository.getAll.returns([]);
+    const repository = new Repository(new Database());
     const sut = new Service(repository)
 
     const result = sut.getAll();
